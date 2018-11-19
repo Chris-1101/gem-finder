b6335fa835d0392558eb5fa6f1f8fa36d4ca6ce7
Natasha Darlington <natasha.darlington@btinternet.com>
2018-11-19 14:06:43 +0000

    added controllers/models and routes

---
+++ db/migrate/20181119135212_create_properties.rb
@@ -0,0 +1,18 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :price
+      t.string :integer
+      t.string :location
+      t.string :string
+      t.string :area_value_url
+      t.string :integer
+      t.string :average_sold_price_1year
+      t.string :integer
+      t.string :crime_rating
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
