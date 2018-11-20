5eed97067e142175e75ee38d76bd2750c6dbf80b
Chris MacBain <cmb.home@live.co.uk>
2018-11-20 20:40:27 +0000

    Update card template style

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -14,18 +14,18 @@ class PropertiesController < ApplicationController
 end
 
 def search_house
+  # properties = []
   url = "https://www.zoopla.co.uk/for-sale/property/london/?q=London&results_sort=newest_listings&search_source=home"
   html_file = open(url).read
   html_doc = Nokogiri::HTML(html_file)
   html_doc.search('.listing-results-wrapper').each do |element|
-    price = element.search('.listing-results-price').text
     name = element.search('.listing-results-attr a').text
     address = element.search('.listing-results-address').text
     photo = element.search('.photo-hover img').attr('src').value
-    @properties << Property.new(name: name, price: price, address: address, photo: photo)
+    description = element.search('.listing-results-attr + p').text
+    price = element.search('.listing-results-price').text
+    @properties << Property.new(name: name, address: address, photo: photo, price: price)
     @properties.take(10)
   end
-    @properties
+  @properties
 end
-
-
