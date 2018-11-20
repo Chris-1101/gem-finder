4aadffc8cdf387337104577d2f45dc1a43a7355b
James Thorp <jamesthorp1@gmail.com>
2018-11-20 12:24:21 +0000

    scraped zoopla data

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,14 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-class House
- attr_reader :name, :price, :href
-  def initialize(name, price, href)
-    @name = name
-    @price = price
-    @href = href
-  end
-end
+
 
 class PropertiesController < ApplicationController
   def index
@@ -28,25 +21,19 @@ def search_house
   url = "https://www.zoopla.co.uk/for-sale/property/london/?q=London&results_sort=newest_listings&search_source=home"
   html_file = open(url).read
   html_doc = Nokogiri::HTML(html_file)
-  html_doc.search('.listing-results-right').each do |element|
-    price = element.search('.listing-results-price').text.strip
-    href = element.search('.listing-results-attr').css('a').first['href']
+  html_doc.search('.listing-results-wrapper').each do |element|
+    price = element.search('.listing-results-price').text
+    name = element.search('.listing-results-attr a').text
+    address = element.search('.listing-results-address').text
+    photo = element.search('.photo-hover img').attr('src').value
 
-    @properties << House.new(, price, href)
+
+
+
+    @properties << Property.new(name: name, price: price, address: address, photo: photo)
     @properties.take(10)
   end
-  display_properties(@properties)
+  @properties
 end
 
-def display_properties(property_instances)
-  # View
-  properties = property_instances.take(5)
-  properties.each_with_index do |house, index|
-    gbp_conversion = house.price.gsub('.', '').to_i
-    gbp = (gbp_conversion * 0.00025)
-    puts "#{index + 1} - #{house.name.text.strip} : #{house.price} (£#{gbp})"
-    # binding.pry
-    puts house.href.to_s
-    puts ''
-  end
-end
+
