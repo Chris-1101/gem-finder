63cb9b29c6c2862c853d9dfac2bb1248b7b5098b
James Thorp <jamesthorp1@gmail.com>
2018-11-20 12:24:57 +0000

    zoopla scrape info

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,8 +2,6 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-
-
 class PropertiesController < ApplicationController
   def index
     @properties = []
@@ -15,9 +13,7 @@ class PropertiesController < ApplicationController
   end
 end
 
-
 def search_house
-
   url = "https://www.zoopla.co.uk/for-sale/property/london/?q=London&results_sort=newest_listings&search_source=home"
   html_file = open(url).read
   html_doc = Nokogiri::HTML(html_file)
@@ -26,14 +22,10 @@ def search_house
     name = element.search('.listing-results-attr a').text
     address = element.search('.listing-results-address').text
     photo = element.search('.photo-hover img').attr('src').value
-
-
-
-
     @properties << Property.new(name: name, price: price, address: address, photo: photo)
     @properties.take(10)
   end
-  @properties
+    @properties
 end
 
 
