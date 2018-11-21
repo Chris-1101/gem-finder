f9f7c19e74c40070262c266cfaed383dc8c70129
Chris MacBain <cmb.home@live.co.uk>
2018-11-21 17:05:31 +0000

    Configure sidekiq

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module RailsGemfinder
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Configure sidekiq to handle jobs queue
+    config.active_job.queue_adapter = :sidekiq
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
