83651d4d4e3fd6ca1781362c0172ffd79e86b1bd
Chris MacBain <cmb.home@live.co.uk>
2018-11-21 00:24:30 +0000

    Update properties table migration

--- db/migrate/20181119135212_create_properties.rb
+++ db/migrate/20181119135212_create_properties.rb
@@ -1,16 +1,16 @@
 class CreateProperties < ActiveRecord::Migration[5.2]
   def change
     create_table :properties do |t|
-      t.string :price
-      t.string :integer
-      t.string :location
       t.string :name
-      t.string :area_value_url
-      t.string :integer
+      t.string :price
+      t.string :address
+      t.string :location
+      t.text :description
+      t.string :photo
       t.string :average_sold_price_1year
-      t.string :integer
+      t.string :area_value_url
       t.string :crime_rating
-      t.string :integer
+      t.string :year
 
       t.timestamps
     end
