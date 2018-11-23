4dd2c8467e1992ae54e0765acc72775ca2b105d2
James Thorp <jamesthorp1@gmail.com>
2018-11-23 12:03:05 +0000

    fixed Chris' error

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,7 +6,7 @@ class Property < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   # Mapbox
-  geocoded_by :postcode
-  after_validation :geocode, if: :will_save_change_to_postcode?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
 end
