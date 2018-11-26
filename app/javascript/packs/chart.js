e5bf23088295ad209dc41c821d2b1b83c9c02adf
Natasha Darlington <natasha.darlington@btinternet.com>
2018-11-26 13:58:32 +0000

    graphs

--- app/javascript/packs/chart.js
+++ app/javascript/packs/chart.js
@@ -68,6 +68,45 @@ const myBarChart = new Chart(ctx2, {
     }
 });
 
+const ctx3 = document.getElementById("myBarCharts").getContext('2d');
+const footDataset = document.getElementById("myBarCharts").dataset.avgPriceFoot.split(',')
+const myBarCharts = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: ["Detached", "Semi-Detached", "Terraced", "Flats"],
+        datasets: [{
+            label: 'Average current value of properties',
+            data: [footDataset[0] * 1000 , footDataset[1] * 1000, footDataset[2] * 1000, footDataset[3] * 1000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 255, 0, 0.2)',
+                'rgba(0, 255,0, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    callback: (value) => {
+                      return '£' + value
+                    }
+                },
+            }]
+        }
+    }
+});
+
+
+
 
 new Chart(document.getElementById("lineGraph"), {
     type: 'line',
